c690a57fbab00310e82183e2699f9cf9f2a9fbad
BrownPharadol <brown01945@gmail.com>
2021-08-26 15:37:26 +0700

    circleBtn

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const carouselSlide = document.querySelector(".carousel-slide");
 const carouselContainer = document.querySelector(".carousel-container");
 
 // define src img array
-const numberOfImages = 7;
+const numberOfImages = 3;
 const srcImages = [];
 for (let index = 1; index <= numberOfImages; index++) {
   srcImages.push(`image/natural_wander${index}.jpg`);
@@ -23,24 +23,58 @@ for (let index = 0; index <= srcImages.length - 1; index++) {
   const image = document.createElement("img");
   image.src = srcImages[index];
   let idImg = "";
-  index == 0? (idImg = "lastClone"): 
-  index == srcImages.length - 1 ? (idImg = "firstClone") : idImg = index;
+  index == 0
+    ? (idImg = "lastClone")
+    : index == srcImages.length - 1
+    ? (idImg = "firstClone")
+    : (idImg = index);
   image.setAttribute("id", idImg);
   carouselSlide.append(image);
 }
+
+//create element circle button
+const circleBtnContainer = document.querySelector(".circleBtnContainer");
+for (let index = 0; index < numberOfImages; index++) {
+  console.log("create circle Button : " + index);
+  circleBtn = document.createElement("i");
+  circleBtn.classList.add("fas");
+  circleBtn.classList.add("fa-circle");
+  circleBtn.setAttribute("id", index+1);
+  circleBtnContainer.append(circleBtn);
+}
+const circleBtns = document.querySelectorAll(".fa-circle");
+console.log(circleBtns)
+
+//Counter
+let counter = 1;
+
+
+function clickCircleBtn(event) {
+  event.target.classList.add("clicked");
+  carouselSlide.style.transform =
+    "translateX(" + -sizeContainer * event.target.id + "px)";
+  carouselSlide.style.transition = "transform 0.5s ease-in-out";
+  counter=event.target.id;
+  console.log(counter)
+}
+
+circleBtns.forEach((btn)=>{
+  btn.addEventListener('click',clickCircleBtn)
+})
+
+
 const carouselImages = document.querySelectorAll(".carousel-slide img");
 
 //Buttons
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
-//Counter
-let counter = 1;
+
 let sizeContainer = carouselContainer.clientWidth;
 
-function slideImages() {
+function slideImages(count) {
   carouselSlide.style.transform =
-    "translateX(" + -sizeContainer * counter + "px)";
+    "translateX(" + -sizeContainer * count + "px)";
 }
 slideImages();
 
@@ -48,17 +82,19 @@ slideImages();
 nextBtn.addEventListener("click", () => {
   if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = "transform 0.5s ease-in-out";
+  console.log("from counter : "+counter+"next");
   counter++;
-  console.log("next to img counter " + counter);
-  carouselSlide.style.transform = slideImages();
+  console.log("to counter " + counter+"next");
+  carouselSlide.style.transform = slideImages(counter);
 });
 
 prevBtn.addEventListener("click", () => {
   if (counter <= 0) return;
+  console.log("from counter : " + counter + "prev");
   carouselSlide.style.transition = "transform 0.5s ease-in-out";
-  console.log("prev to img counter" + counter);
+  console.log("to counter " + counter + "prev");
   counter--;
-  carouselSlide.style.transform = slideImages();
+  carouselSlide.style.transform = slideImages(counter);
 });
 
 // Carousel Images
@@ -67,10 +103,9 @@ carouselSlide.addEventListener("webkitTransitionEnd", () => {
   if (carouselImages[counter].id === "lastClone") {
     console.log("transition slide is working");
     console.log(carouselImages[counter]);
-
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - 2;
-    carouselSlide.style.transform = slideImages();
+    carouselSlide.style.transform = slideImages(counter);
   }
   if (carouselImages[counter].id === "firstClone") {
     console.log("transition slide is working");
@@ -79,8 +114,8 @@ carouselSlide.addEventListener("webkitTransitionEnd", () => {
     console.log("imagesLength : " + carouselImages.length);
 
     carouselSlide.style.transition = "none";
-    counter = carouselImages.length - counter; 
-    carouselSlide.style.transform = slideImages();
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = slideImages(counter);
   }
 });
 
@@ -88,5 +123,5 @@ carouselSlide.addEventListener("webkitTransitionEnd", () => {
 window.onresize = function () {
   sizeContainer = carouselContainer.clientWidth;
   console.log("resize container to" + sizeContainer);
-  carouselSlide.style.transform = slideImages();
+  carouselSlide.style.transform = slideImages(counter);
 };
