1a2907cf651a841562b773374fbffcf382cf233b
PharadolBrown <87072853+PharadolBrown@users.noreply.github.com>
2021-08-28 15:06:13 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,55 +1,64 @@
-const carouselSlide = document.querySelector(".carousel-slide");
+// To anyone are reading my code
+// My code is dynamic. you can easily add or reduce images in the collection just 1 line only!!
+// StepI : increase or decrease the value of a variable named "numberOfImages" (line : 7)
 const carouselContainer = document.querySelector(".carousel-container");
-
-// define src img array
-const numberOfImages = 7;
+const carouselSlide = document.querySelector(".carousel-slide");
+const circleBtnContainer = document.querySelector(".circleBtnContainer");
+const numberOfImages = 11; //---- increase the value up to 12. Because there're 11 images in folder image
 const srcImages = [];
 
-for (let index = 1; index <= numberOfImages; index++) {
-  srcImages.push(`image/natural_wonder${index}.jpg`);
-}
-// clone first & last image
-const lastCloneSrc = srcImages.length;
-srcImages.unshift(`image/natural_wonder${lastCloneSrc}.jpg`);
-const firstCloneSrc = 1;
-srcImages.push(`image/natural_wonder${firstCloneSrc}.jpg`);
+createImages();
+createCircleBtns();
 
-//create element img & add first , last clone class to some img
-for (let index = 0; index <= srcImages.length - 1; index++) {
-  const image = document.createElement("img");
-  image.src = srcImages[index];
-  let idImg = "";
-  index == 0
-    ? (idImg = "lastClone")
-    : index == srcImages.length - 1
-    ? (idImg = "firstClone")
-    : (idImg = index);
-  image.setAttribute("id", idImg);
-  carouselSlide.append(image);
-}
-const carouselImages = document.querySelectorAll(".carousel-slide img"); // get images
+function createImages() {
+  // define src img array
+  for (let index = 1; index <= numberOfImages; index++) {
+    srcImages.push(`image/natural_wonder${index}.jpg`);
+  }
+  // clone first & last image
+  const lastCloneSrc = srcImages.length;
+  srcImages.unshift(`image/natural_wonder${lastCloneSrc}.jpg`);
+  const firstCloneSrc = 1;
+  srcImages.push(`image/natural_wonder${firstCloneSrc}.jpg`);
 
-//create element circle button
-const circleBtnContainer = document.querySelector(".circleBtnContainer");
-for (let index = 0; index < numberOfImages; index++) {
-  circleBtn = document.createElement("i");
-  circleBtn.classList.add("fas");
-  circleBtn.classList.add("fa-circle");
-  circleBtn.setAttribute("id", index + 1);
-  circleBtnContainer.append(circleBtn);
+  //create element img & add first , last clone class to some img
+  for (let index = 0; index <= srcImages.length - 1; index++) {
+    const image = document.createElement("img");
+    image.src = srcImages[index];
+    let idImg = "";
+    index == 0
+      ? (idImg = "lastClone")
+      : index == srcImages.length - 1
+      ? (idImg = "firstClone")
+      : (idImg = index);
+    image.setAttribute("id", idImg);
+    carouselSlide.append(image);
+  }
+}
+const carouselImages = document.querySelectorAll(".carousel-slide img"); // get all images
+
+function createCircleBtns() {
+  for (let index = 0; index < numberOfImages; index++) {
+    circleBtn = document.createElement("i");
+    circleBtn.classList.add("fas");
+    circleBtn.classList.add("fa-circle");
+    circleBtn.setAttribute("id", index + 1);
+    circleBtnContainer.append(circleBtn);
+  }
 }
 const circleBtns = document.querySelectorAll(".fa-circle");
 
+const transformAnimation = "transform 0.5s ease-in-out";
+// event circle buttons
+circleBtns.forEach((btn) => {
+  btn.addEventListener("click", clickCircleBtn);
+});
 function clickCircleBtn(event) {
-  // event.target.classList.add("clicked");
   slideImages(event.target.id);
-  carouselSlide.style.transition = "transform 0.5s ease-in-out";
-  console.log("circle count from " + counter);
+  carouselSlide.style.transition = transformAnimation;
   counter = event.target.id;
-  console.log("circle count to " + counter);
   focusCircleBtn();
 }
-
 function focusCircleBtn() {
   for (let index = 1; index <= numberOfImages; index++) {
     if (counter == index) {
@@ -60,69 +69,41 @@ function focusCircleBtn() {
   }
 }
 
-circleBtns.forEach((btn) => {
-  btn.addEventListener("click", clickCircleBtn);
-  //
-});
-
-console.log("w container" + carouselContainer.clientWidth);
-console.log("w slide" + carouselSlide.clientWidth);
-
-//Counter
-let counter = 4;
-console.log("define counter : " + counter);
-
-//Buttons
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
-
+// animation slide
 let sizeContainer = carouselContainer.clientWidth;
-// let sizeContainer = carouselImages[0].clientWidth*carouselImages.length;
-
-for (let index = 0; index < carouselImages.length; index++) {
-  console.log(
-    "id img " +
-      carouselImages[index].id +
-      "width" +
-      carouselImages[index].clientWidth
-  );
-}
-
+let counter = Math.ceil(numberOfImages / 2);
 function slideImages(count) {
-  console.log(" now counter : " + count);
   carouselSlide.style.transform =
     "translateX(" + -sizeContainer * count + "px)";
-  console.log("-sizeContainer" + sizeContainer + "*" + count);
   focusCircleBtn();
 }
 slideImages(counter);
 
-//Button Listeners
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+//Event Arrow Button
 nextBtn.addEventListener("click", () => {
   if (counter >= carouselImages.length - 1) return;
-  carouselSlide.style.transition = "transform 0.5s ease-in-out";
+  carouselSlide.style.transition = transformAnimation;
   counter++;
   slideImages(counter);
 });
 
 prevBtn.addEventListener("click", () => {
   if (counter <= 0) return;
-  carouselSlide.style.transition = "transform 0.5s ease-in-out";
+  carouselSlide.style.transition = transformAnimation;
   counter--;
   slideImages(counter);
 });
 
-// Carousel Images
+// Carousel Images Mirage
 carouselSlide.addEventListener("webkitTransitionEnd", () => {
   if (carouselImages[counter].id === "lastClone") {
-    console.log("transition slide is working");
-    console.log(carouselImages[counter]);
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - 2;
     slideImages(counter);
   }
   if (carouselImages[counter].id === "firstClone") {
-    console.log("transition slide is working");
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - counter;
     slideImages(counter);
